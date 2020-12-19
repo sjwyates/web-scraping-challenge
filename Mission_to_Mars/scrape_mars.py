1a4ccfa29c0f63345659ab3f63b779f93dc8cfe4
Stuart Yates <sjwyates@Mac-mini.local>
2020-12-18 20:36:29 -0700

    wrote scrapy function

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,78 @@
+from splinter import Browser
+from bs4 import bs as bs
+import pandas as pd
+import requests
+import time
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+def init_browser():
+    executable_path = {'executable_path': ChromeDriverManager().install()}
+    return Browser('chrome', **executable_path, headless=False)
+
+
+def scrape_info():
+    # oh the places you'll go...
+    urls = {
+        'nasa_news': 'https://mars.nasa.gov/news/',
+        'nasa_jpl': { 'base': 'https://www.jpl.nasa.gov', 'query': '/spaceimages/?search=&category=Mars' },
+        'mars_facts': 'https://space-facts.com/mars/',
+        'astrogeology': { 'base': 'https://astrogeology.usgs.gov', 'query': '/search/results?q=hemisphere+enhanced&k1=target&v1=Mars' }
+    }
+
+    # helper function
+    def visit_wait_return(url):
+        browser.visit(url)
+        time.sleep(5)
+        html = browser.html
+        return bs(html, 'html.parser')
+
+    # open up a browser (to be used for everything)
+    browser = init_browser()
+
+    # first visit
+    nasa_news_soup = visit_wait_return(urls['nasa_news'])
+    articles = nasa_news_soup.find_all('div', class_='image_and_description_container')
+    article_1_title = articles[0].find('h3').get_text(strip=True)
+    article_1_text = articles[0].find('div', class_='article_teaser_body').get_text(strip=True)
+
+    # second visit
+    nasa_jpl_soup = visit_wait_return(urls['nasa_jpl']['base'] + urls['nasa_jpl']['query'])
+    featured_img_url = urls['nasa_jpl']['base'] + nasa_jpl_soup.find('article', class_='carousel_item').attrs['style'].split("'")[1]
+
+    # third visit
+    mars_facts_df = pd.DataFrame(pd.read_html(urls['mars_facts'])[0])
+    mars_facts_html = mars_facts_df.to_html()
+
+    # fourth visit
+    astrogeology_soup = visit_wait_return(urls['astrogeology']['base'] + urls['astrogeology']['query'])
+    items = astrogeology_soup.find_all('div', class_='item')
+    links_to_results = []
+    for item in items:
+        link = item.find('a', class_='itemLink product-item')['href']
+        title = item.find('h3').get_text(strip=True)
+        links_to_results.append({ 'link': link, 'title': title })
+    links_to_images = []
+    for link in links_to_results:
+        this_page_soup = visit_wait_return(link)
+        downloads = this_page_soup.find('div', class_='downloads')
+        img_url = downloads.find('ul').find_all('a')[1]['href']
+        links_to_images.append({ 'title': link['title'], 'img_url': img_url })
+
+    browser.quit()
+
+    return {
+        'nasa_news': {
+            'title': article_1_title,
+            'text': article_1_text
+        },
+        'nasa_jpl': {
+            'featured_img_url': featured_img_url,
+        },
+        'mars_facts': {
+            'html': mars_facts_html
+        },
+        'astrogeology': {
+            'img_urls': links_to_images
+        }
+    }
